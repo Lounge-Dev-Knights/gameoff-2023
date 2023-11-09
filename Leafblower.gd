f3f6a6b085362e52dcfca25e69fc4ae14440a7bf
Raphael Däster <33464588+Alpha2412@users.noreply.github.com>
2023-11-09 19:15:27 +0100

    Force of leafblower changed
    Force linear regressive to distance

--- Leafblower.gd
+++ Leafblower.gd
@@ -3,12 +3,12 @@ extends RigidBody2D
 
 var density := 0.001 # kg per pixel
 var size := 64.0
-var blow_force = 500000000.0  # Adjust as needed
+var blow_force = 5000.0  # Adjust as needed
 
 
 func _physics_process(delta):
 	for body in $Area2D.get_overlapping_bodies():
-		var distance_square = pow((body.global_position - global_position).length(),2)
-		body.apply_central_force(Vector2(blow_force/distance_square,0))
+		var distance = (body.global_position - global_position).length()
+		body.apply_central_force(Vector2(blow_force-((3000-distance)*0.5),0))
 
 
