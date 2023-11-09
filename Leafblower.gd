481a9f77444fe8f2b9d6da3bc21e13ff8922ae0b
Raphael Däster <33464588+Alpha2412@users.noreply.github.com>
2023-11-09 22:00:37 +0100

    Leafblower blows in direction its  pointing

--- Leafblower.gd
+++ Leafblower.gd
@@ -3,12 +3,17 @@ extends RigidBody2D
 
 var density := 0.001 # kg per pixel
 var size := 64.0
-var blow_force = 5000.0  # Adjust as needed
+var blow_force = 7500.0  # Adjust as needed
 
+func _ready() -> void:
+	mass = size * size * density
 
 func _physics_process(delta):
 	for body in $Area2D.get_overlapping_bodies():
 		var distance = (body.global_position - global_position).length()
-		body.apply_central_force(Vector2(blow_force-((3000-distance)*0.5),0))
+		rotation
+		var direction = Vector2.RIGHT.rotated(rotation)
+		var force = blow_force-((3000-distance)*0.5)
+		body.apply_central_force(direction*force)
 
 
