fcd726e08336988b644e5b45f49a971235447b41
Raphael Däster <33464588+Alpha2412@users.noreply.github.com>
2023-11-09 22:13:08 +0100

    Made leafblower blow only when held

--- Leafblower.gd
+++ Leafblower.gd
@@ -9,11 +9,13 @@ func _ready() -> void:
 	mass = size * size * density
 
 func _physics_process(delta):
-	for body in $Area2D.get_overlapping_bodies():
-		var distance = (body.global_position - global_position).length()
-		rotation
-		var direction = Vector2.RIGHT.rotated(rotation)
-		var force = blow_force-((3000-distance)*0.5)
-		body.apply_central_force(direction*force)
+	if freeze:
+		for body in $Area2D.get_overlapping_bodies():
+			var distance = (body.global_position - global_position).length()
+			rotation
+			var direction = Vector2.RIGHT.rotated(rotation)
+			var force = blow_force-((3000-distance)*0.5)
+			body.apply_central_force(direction*force)
+
 
 
