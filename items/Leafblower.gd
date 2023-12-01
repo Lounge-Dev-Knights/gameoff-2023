70dd125e41c408f8d012909a7e73dd2ecaa418ca
Dardan Bujupaj <dardan@ax.tech>
2023-12-01 22:52:02 +0100

    remove leafblower afert 5s usage

--- items/Leafblower.gd
+++ items/Leafblower.gd
@@ -1,11 +1,25 @@
 extends RigidBody2D
 
-
+var power := 5.0
 var blow_force = 3000.0  # Adjust as needed
 
 
+@onready var progress_bar := $ProgressBar
+
+func _process(delta: float) -> void:
+	progress_bar.visible = freeze
+	progress_bar.value = power
+	progress_bar.global_position = global_position + Vector2(-32, 0)
+	
+	if freeze:
+		power -= delta
+		if power < 0.0:
+			queue_free()
+			get_tree().set_group("character", "hold_item", null)
+
 func _physics_process(delta):
 	if freeze:
+		
 		for body in $Area2D.get_overlapping_bodies():
 			var distance = (body.global_position - global_position).length()
 			var direction = Vector2.RIGHT.rotated(rotation)
